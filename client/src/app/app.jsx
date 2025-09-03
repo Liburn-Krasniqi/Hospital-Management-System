29330e83300f9340d9c1122a329aad89aebbfc89
Liburn <liburn@macBook-Leblebia.local>
2025-09-03 10:49:28 +0200

    Navigation Add to general layout

--- client/src/app/app.jsx
+++ client/src/app/app.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import "bootstrap/dist/css/bootstrap.css";
 import { AppProvider } from './provider';
 import { AppRouter } from './router';
 import { AppRoutes } from './routes';
